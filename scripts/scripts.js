c01b09dcd78d0a3526ed0d74ada9741e3a870f91
JustANipple <prandisamuel99@gmail.com>
2023-05-19 15:54:54 +0200

    modified:   index.html 	modified:   scripts/scripts.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/base/_page.scss 	modified:   styles/scss/components/_donation.scss 	modified:   styles/scss/components/_navBar.scss

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,18 @@
 const dropdownBtn = document.querySelector(".menu_dropdown_btn");
 const dropdownMenu = document.querySelector(".menu_options");
 const bookmarkBtn = document.querySelector(".button_bookmark");
+const navBar = document.querySelector(".nav_bar");
 
 dropdownMenu.style.transition = "all .25s ease-in-out";
 bookmarkBtn.style.transition = "all .25s ease-in-out";
 
 dropdownBtn.addEventListener("click", () => {
     if(dropdownBtn.classList.contains("closeMenu")) {
+        navBar.classList.remove("darkLayer");
         dropdownMenu.classList.remove("openMenu");
         dropdownBtn.classList.remove("closeMenu");
     } else {
+        navBar.classList.add("darkLayer");
         dropdownBtn.classList.add("closeMenu");
         dropdownMenu.classList.add("openMenu");
     }
