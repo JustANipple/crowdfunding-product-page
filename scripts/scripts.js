5ff021e71809298808298ad3183c5d878cb64c5e
JustANipple <prandisamuel99@gmail.com>
2023-05-22 10:07:42 +0200

    modified:   index.html 	modified:   scripts/scripts.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/components/_header.scss 	modified:   styles/scss/components/_navBar.scss

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,14 @@
 const dropdownBtn = document.querySelector(".menu_dropdown_btn");
 const dropdownMenu = document.querySelector(".menu_options");
 const bookmarkBtn = document.querySelector(".button_bookmark");
+const iconCircle = bookmarkBtn.querySelector("circle");
+const iconPath = bookmarkBtn.querySelector("path");
 const navBar = document.querySelector(".nav_bar");
+const backProjectBtn = document.querySelector(".button_back_project");
 
 dropdownMenu.style.transition = "all .25s ease-in-out";
 bookmarkBtn.style.transition = "all .25s ease-in-out";
+backProjectBtn.style.transition = "all .25s ease-in-out";
 
 dropdownBtn.addEventListener("click", () => {
     if(dropdownBtn.classList.contains("closeMenu")) {
@@ -17,3 +21,4 @@ dropdownBtn.addEventListener("click", () => {
         dropdownMenu.classList.add("openMenu");
     }
 });
+
